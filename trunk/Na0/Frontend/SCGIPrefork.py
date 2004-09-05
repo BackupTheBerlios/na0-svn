1b2937afad735fd473ae316ce5bb2a416d278401
perky <perky@3f1e9d57-5ae3-0310-a4e1-a9e052ff1551>
2004-09-05 23:09:31 +0000

    Rename scgi to SCGIPrefork to avoid module name confict agains scgi package.

---
+++ trunk/Na0/Frontend/SCGIPrefork.py
@@ -0,0 +1,38 @@
+#
+# Na0/Frontend/SCGIPrefork.py
+#  - scgi frontend adapter
+#
+# $Id$
+#
+
+__all__ = ['Na0SCGIHandler']
+__docformat__ = 'epydoc'
+
+import sys
+from scgi import scgi_server
+
+class Na0SCGIHandler(scgi_server.SCGIHandler):
+
+    def handle_connection(self, conn):
+        input = conn.makefile("r")
+        output = conn.makefile("w")
+
+        env = self.read_env(input)
+        output.write("Content-Type: text/plain\r\n")
+        output.write("\r\n")
+        for k, v in env.items():
+            output.write("[%s] %r\n" % (k, v))
+
+        output.close()
+        input.close()
+        conn.close()
+
+def main():
+    if len(sys.argv) == 2:
+        port = int(sys.argv[1])
+    else:
+        port = 6942 # default port for Na0
+    scgi_server.SCGIServer(Na0SCGIHandler, port=port).serve()
+
+if __name__ == '__main__':
+    main()
--- trunk/Na0/Frontend/scgi.py
+++
@@ -1,9 +0,0 @@
-#
-# Na0/Frontend/scgi.py
-#  - scgi frontend adapter
-#
-# $Id$
-#
-
-__all__ = []
-__docformat__ = 'epydoc'
