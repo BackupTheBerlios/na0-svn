2c32ea8caf0640d1e2951b6adb8ee347e2fafe60
perky <perky@3f1e9d57-5ae3-0310-a4e1-a9e052ff1551>
2004-09-07 16:09:27 +0000

    Add new Na0.Template and Na0.Configuration.

---
+++ trunk/Na0/Configuration.py
@@ -0,0 +1,8 @@
+# Na0/Configuration.py
+# $Id$
+
+"""Configuration management
+"""
+
+__all__ = []
+__docformat__ = 'epytext'
---
+++ trunk/Na0/Template.py
@@ -0,0 +1,10 @@
+# Na0/Template.py
+# $Id$
+
+"""Templating interface for Cheetah template
+"""
+
+__all__ = []
+__docformat__ = 'epytext'
+
+from Cheetah.Template import Template
