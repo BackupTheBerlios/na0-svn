977878f61a99f303bc63e98d508af2d9be2568bf
perky <perky@3f1e9d57-5ae3-0310-a4e1-a9e052ff1551>
2004-09-08 16:46:30 +0000

    Add na0d script for daemon type launchers.

---
+++ trunk/Na0/Scripts/__init__.py
@@ -0,0 +1,5 @@
+# Na0/Scripts/__init__.py
+# $Id: __init__.py 8 2004-09-06 00:03:09Z perky $
+
+"""NaYeong scripts for command line interface
+"""
---
+++ trunk/Na0/Scripts/na0d.py
@@ -0,0 +1,8 @@
+# Na0/Scripts/na0d.py
+# $Id: Template.py 17 2004-09-07 16:09:27Z perky $
+
+"""Launcher command line interface for daemon type frontends
+"""
+
+__all__ = []
+__docformat__ = 'epytext'
