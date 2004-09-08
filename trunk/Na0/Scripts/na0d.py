a69ee333f7104bf38454de48de83c33eb1ae9aa6
perky <perky@3f1e9d57-5ae3-0310-a4e1-a9e052ff1551>
2004-09-08 17:18:25 +0000

    Implement Na0.Scripts.na0d little bit.

--- trunk/Na0/Scripts/na0d.py
+++ trunk/Na0/Scripts/na0d.py
@@ -6,3 +6,45 @@
 
 __all__ = []
 __docformat__ = 'epytext'
+
+import sys, os
+
+def parse_options():
+    """Parses command line options
+
+    @return: parsed option and arguments
+    @rtype: tuple that contains (options, args)
+    """
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option("-c", "--config", dest="configfile", default='na0.ini',
+                      help="configuration to read", metavar="FILE")
+    parser.add_option("-f", "--foreground",
+                      action="store_false", dest="daemonize", default=True,
+                      help="don't go background")
+
+    return parser.parse_args()
+
+def parse_config(cfgfile):
+    """Parses basic configuration file.
+
+    The configuration file will have only few bits that can't be changed
+    while a daemon is running. (eg. internal encoding and backend storage
+    setup)
+
+    @return: config instance with parsed configurations set.
+    @rtype: L{Config} object
+    """
+    from ConfigParser import ConfigParser
+
+    parser = ConfigParser()
+    parser.read(cfgfile)
+    # TODO: do some parse stuff
+
+def main():
+    """Runs main block of `na0d'.
+    """
+    (options, args) = parse_options()
+    parse_config(options.configfile)
+    # TODO: do rest of initializations
