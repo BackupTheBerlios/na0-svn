5d0af85edd9ee693aea81209e64267f4904068a7
perky <perky@3f1e9d57-5ae3-0310-a4e1-a9e052ff1551>
2004-09-08 16:12:34 +0000

    Add generic storage and postgresql backend.

---
+++ trunk/Na0/Storage/Generic.py
@@ -0,0 +1,11 @@
+# Na0/Storage/Generic.py
+# $Id$
+
+"""Generic Storage Backend (just for interface)
+"""
+
+__all__ = []
+__docformat__ = 'epytext'
+
+class GenericStorage:
+    pass
---
+++ trunk/Na0/Storage/PostgreSQL.py
@@ -0,0 +1,13 @@
+# Na0/Storage/PostgreSQL.py
+# $Id$
+
+"""PostgreSQL Storage Backend
+"""
+
+__all__ = []
+__docformat__ = 'epytext'
+
+from Na0.Storage.Generic import GenericStorage
+
+class PostgreSQLStorage(GenericStorage):
+    pass
